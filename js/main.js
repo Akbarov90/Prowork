d5758643ee5d1cce538b9f83e0508d339965e866
Akbarov90 <ilyasovich706@gmail.com>
2021-06-17 21:56:01 +0500

    prowork-landing-page

---
+++ js/main.js
@@ -0,0 +1,8 @@
+const button = document.querySelector("#chk");
+button.addEventListener("change",()=>{
+   document.body.classList.toggle("dark-theme")
+});
+const button2 = document.querySelector("#chk1");
+button2.addEventListener("change",()=>{
+   document.body.classList.toggle("dark-theme")
+});
